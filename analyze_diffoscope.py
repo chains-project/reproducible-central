7fef9c3b36d97da8474a01ef1fcd6657854b57cf
Aman Sharma <mannu.poski10@gmail.com>
2024-10-08 20:56:31 +0200

    chore: analyse diffoscope outputs better

--- analyze_diffoscope.py
+++ analyze_diffoscope.py
@@ -5,21 +5,33 @@ import os
 import json
 import sys
 
-count_of_tools = {'procyon': set(), 'javap': set()}
+count_of_tools = {'procyon': {}, 'javap': {}}
 
-def process_details(details, group_id, artifact_id, count_of_tools, version):
+def print_count_of_tools():
+    for key in count_of_tools:
+        print(f"## {key}")
+        for tool in count_of_tools[key]:
+            print(f"### {tool}")
+            for diffoscope_file in count_of_tools[key][tool]:
+                print(f"- {diffoscope_file}")
+
+def process_details(details, group_id, artifact_id, count_of_tools, version, diffoscope_file):
     for detail in details:
         source1 = detail.get('source1', '')
         source2 = detail.get('source2', '')
-        
+
         if 'procyon' in source1 or 'procyon' in source2:
-            count_of_tools['procyon'].add(f"{group_id}:{artifact_id}:{version}")
+            if f"{group_id}:{artifact_id}:{version}" not in count_of_tools['procyon']:
+                count_of_tools['procyon'][f"{group_id}:{artifact_id}:{version}"] = set()
+            count_of_tools['procyon'][f"{group_id}:{artifact_id}:{version}"].add(diffoscope_file)
         if 'javap' in source1 or 'javap' in source2:
-            count_of_tools['javap'].add(f"{group_id}:{artifact_id}:{version}")
+            if f"{group_id}:{artifact_id}:{version}" not in count_of_tools['javap']:
+                count_of_tools['javap'][f"{group_id}:{artifact_id}:{version}"] = set()
+            count_of_tools['javap'][f"{group_id}:{artifact_id}:{version}"].add(diffoscope_file)
         
         # Recursively process nested details
         if 'details' in detail:
-            process_details(detail['details'], group_id, artifact_id, count_of_tools, version)
+            process_details(detail['details'], group_id, artifact_id, count_of_tools, version, diffoscope_file)
 
 def analyze_diffoscope(diffoscope_file):
     with open(diffoscope_file, 'r') as file:
@@ -31,14 +43,18 @@ def analyze_diffoscope(diffoscope_file):
     group_id = diffoscope_file.parent.parent.parent.name
     if 'details' in data:
         details = data['details']
-        process_details(details, group_id, artifact_id, count_of_tools, version)
+        process_details(details, group_id, artifact_id, count_of_tools, version, diffoscope_file)
     else:
         source1 = data.get('source1', '')
         source2 = data.get('source2', '')
         if 'procyon' in source1 or 'procyon' in source2:
-            count_of_tools['procyon'].add(f"{group_id}:{artifact_id}:{version}")
+            if f"{group_id}:{artifact_id}:{version}" not in count_of_tools['procyon']:
+                count_of_tools['procyon'][f"{group_id}:{artifact_id}:{version}"] = set()
+            count_of_tools['procyon'][f"{group_id}:{artifact_id}:{version}"].add(diffoscope_file)
         if 'javap' in source1 or 'javap' in source2:
-            count_of_tools['javap'].add(f"{group_id}:{artifact_id}:{version}")
+            if f"{group_id}:{artifact_id}:{version}" not in count_of_tools['javap']:
+                count_of_tools['javap'][f"{group_id}:{artifact_id}:{version}"] = set()
+            count_of_tools['javap'][f"{group_id}:{artifact_id}:{version}"].add(diffoscope_file)
 
 if __name__ == "__main__":
     for root, dirs, files in os.walk("results"):
@@ -46,4 +62,4 @@ if __name__ == "__main__":
             if file.endswith(".diffoscope.json"):
                 analyze_diffoscope(os.path.join(root, file))
 
-    print(count_of_tools)
+    print_count_of_tools()

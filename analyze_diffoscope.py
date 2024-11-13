d04914638fa06f06fd7fad5af54044c14f80bf20
Aman Sharma <mannu.poski10@gmail.com>
2024-11-13 16:48:29 +0100

    get artifact name that differ

--- analyze_diffoscope.py
+++ analyze_diffoscope.py
@@ -8,19 +8,23 @@ import argparse
 import shutil
 
 count_of_tools = {'procyon': {}, 'javap': {}}
+artifact_name = ""
 
 def process_details(details, group_id, artifact_id, count_of_tools, version, diffoscope_file):
     for detail in details:
         source1 = detail.get('source1', '')
         source2 = detail.get('source2', '')
-        unified_diff = detail.get('unified_diff', '')
+        candidate = get_artifact_name_or_none(source1)
+        if candidate:
+            artifact_name = candidate
 
         for tool in ['procyon', 'javap']:
             if tool in source1 or tool in source2:
                 key = f"{group_id}:{artifact_id}:{version}"
                 if key not in count_of_tools[tool]:
-                    count_of_tools[tool][key] = {'diffs': set(), 'file': diffoscope_file}
-                count_of_tools[tool][key]['diffs'].add(unified_diff)
+                    count_of_tools[tool][key] = set([artifact_name])
+                else:
+                    count_of_tools[tool][key].add(artifact_name)
         
         # Recursively process nested details
         if 'details' in detail:
@@ -40,23 +44,22 @@ def analyze_diffoscope(diffoscope_file):
     else:
         source1 = data.get('source1', '')
         source2 = data.get('source2', '')
-        unified_diff = data.get('unified_diff', '')
+        candidate = get_artifact_name_or_none(source1)
+        if candidate:
+            artifact_name = candidate
         for tool in ['procyon', 'javap']:
             if tool in source1 or tool in source2:
                 key = f"{group_id}:{artifact_id}:{version}"
                 if key not in count_of_tools[tool]:
-                    count_of_tools[tool][key] = {'diffs': set(), 'file': str(diffoscope_file)}
-                count_of_tools[tool][key]['diffs'].add(unified_diff)
+                    count_of_tools[tool][key] = set([artifact_name])
+                else:
+                    count_of_tools[tool][key].add(artifact_name)
 
-def format_readme(tool, data):
-    readme = f"# {tool.capitalize()} Differences\n\n"
-    for release, info in data.items():
-        readme += f"## {release}\n\n"
-        json_filename = f"{release.replace(':', '_')}.diffoscope.json"
-        readme += f"Diffoscope JSON file: [{json_filename}]({json_filename})\n\n"
-        for diff in info['diffs']:
-            readme += f"```diff\n{diff}\n```\n\n"
-    return readme
+def get_artifact_name_or_none(source):
+    if source.endswith(".jar") or source.endswith(".zip"):
+        return source.split("/")[-1]
+    
+    return None
 
 if __name__ == "__main__":
     for root, dirs, files in os.walk("results"):
@@ -64,27 +67,5 @@ if __name__ == "__main__":
             if file.endswith(".diffoscope.json"):
                 analyze_diffoscope(os.path.join(root, file))
 
-    # Create docs directory if it doesn't exist
-    os.makedirs("docs", exist_ok=True)
+    print(count_of_tools)
 
-    # Create README files for each tool and copy JSON files
-    for tool in ['procyon', 'javap']:
-        readme_content = format_readme(tool, count_of_tools[tool])
-        with open(f"docs/{tool}_README.md", 'w') as f:
-            f.write(readme_content)
-        
-        # Copy and rename JSON files
-        for release, release_info in count_of_tools[tool].items():
-            json_file = release_info['file']
-            new_json_filename = f"{release.replace(':', '_')}.diffoscope.json"
-            shutil.copy2(json_file, os.path.join("docs", new_json_filename))
-
-    print(f"README files and JSON files have been created/copied in the 'docs' directory.")
-    
-    results = {'procyon': {}, 'javap': {}}
-    for key in count_of_tools:
-        results[key]['releases'] = len(count_of_tools[key])
-        results[key]['diffs'] = sum(len(info['diffs']) for info in count_of_tools[key].values())
-    
-    print("Summary of results:")
-    print(json.dumps(results, indent=2))

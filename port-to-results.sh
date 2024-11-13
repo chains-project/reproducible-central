80ad18907f1394146f47c341fdbb641ca0595c24
Aman Sharma <mannu.poski10@gmail.com>
2024-11-13 11:33:21 +0100

    include all versions of same group id and artifact id

--- port-to-results.sh
+++ port-to-results.sh
@@ -7,9 +7,6 @@ source "${SCRIPTDIR}/bin/includes/logging.sh"
 
 content_dir="content"
 
-# Initialize empty associative arrays
-declare -A groupIdSet
-declare -A artifactIdSet
 
 project_count=0
 max_projects=$1
@@ -52,16 +49,6 @@ do
         info "Skipping $buildspec as it's not a Maven project"
         continue
     fi
-
-    # Check if we've already processed this groupId/artifactId combination
-    if [[ -n ${groupIdSet[$groupId]} && -n ${artifactIdSet[$artifactId]} ]]
-    then
-        info "Skipping $buildspec as we have one version of this artifact"
-        continue
-    fi
-
-    groupIdSet[$groupId]=1
-    artifactIdSet[$artifactId]=1
 
     mkdir -p results/$groupId/$artifactId/$version
     cp $buildspec results/$groupId/$artifactId/$version

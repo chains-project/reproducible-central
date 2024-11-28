65cdcc7f78fed54a700c54664984e2d91a502bf2
Aman Sharma <mannu.poski10@gmail.com>
2024-11-28 15:25:39 +0100

    include zip files as well

--- make-release-consistent.py
+++ make-release-consistent.py
@@ -27,36 +27,26 @@ def check_maven_central(group_id, artifact_id, version):
     return response.status_code == 200
 
 def extract_artifact_id(artifact_name, version):
-    # Only process .jar files
-    if not artifact_name.endswith('.jar'):
-        return None
+    # Get the extension
+    for ext in ['.zip','.jar']:
+        if artifact_name.endswith(ext):
+            # Find where version starts
+            version_start = artifact_name.find(f"-{version}")
+            if version_start != -1:
+                # Keep everything before version, don't add extension
+                return artifact_name[:version_start]
     
-    # Remove the version and .jar extension, and handle sources.jar
-    cleaned_name = re.sub(r'-test-sources.jar', '.jar', artifact_name)
-    cleaned_name = re.sub(r'-sources.jar', '.jar', cleaned_name)
-    cleaned_name = re.sub(r'-tests.jar', '.jar', cleaned_name)
-    cleaned_name = re.sub(r'-javadoc.jar', '.jar', cleaned_name)
-    cleaned_name = re.sub(r'-jar.jar', '.jar', cleaned_name)
-    cleaned_name = re.sub(r'-shaded.jar', '.jar', cleaned_name)
-    cleaned_name = re.sub(r'-standalone.jar', '.jar', cleaned_name)
-    cleaned_name = re.sub(r'-osgi.jar', '.jar', cleaned_name)
-    cleaned_name = cleaned_name.replace(f"-{version}", "")
-    cleaned_name = cleaned_name.replace(".jar", "")
-    return cleaned_name
+    return None
+    
 
-def total_classfiles(root_dir, artifact_name, source):
-    path = os.path.join(root_dir, source.value, artifact_name)
-    try:
-        jar_file = zipfile.ZipFile(path)
-        return sum(1 for _ in jar_file.namelist() if _.endswith('.class'))
-    except FileNotFoundError:
-        logging.warning(f"Artifact {artifact_name} missing {source.value}")
+    # Check if both files exist and get their sizes
+    if os.path.exists(ref_path):
+        ref_size = os.path.getsize(ref_path)
+        return ref_size
+    else:
+        logging.warning(f"Missing {source} file: {ref_path}")
         return 0
 
-def get_artifact_size(root_dir, artifact_name, source):
-    # Get paths for both reference and rebuild
-    ref_path = os.path.join(root_dir, source.value, artifact_name)
-    
     # Check if both files exist and get their sizes
     if os.path.exists(ref_path):
         ref_size = os.path.getsize(ref_path)
@@ -69,8 +59,6 @@ def process_jnorm_summaries():
     base_dir = "from-repairnator"
     gav_to_artifact_map = {}
     gav_to_file_path_map = {}
-    reference_total_size = 0
-    rebuild_total_size = 0
 
     for root, _, files in os.walk(base_dir):
         for file in files:
@@ -90,7 +78,7 @@ def process_jnorm_summaries():
                                 artifact_name = artifact.get("artifact_name", "")
                                 
                                 # Skip if not a jar file
-                                if not artifact_name.endswith('.jar'):
+                                if not artifact_name.endswith('.jar') and not artifact_name.endswith('.zip'):
                                     continue
                                 
                                 # Verify reference and rebuild are same
@@ -100,8 +88,15 @@ def process_jnorm_summaries():
 
 
                                 extracted_artifact_id = extract_artifact_id(artifact_name, version)
-                                if group_id == "org.apache.cxf.fediz" and extracted_artifact_id == "common":
-                                    modified_group_id = "org.apache.cxf.fediz.examples.wsclientWebapp.webservice"
+                                if group_id == "org.apache.cxf.fediz":
+                                    possible_group_ids = ["org.apache.cxf.fediz", "org.apache.cxf.fediz.examples.wsclientWebapp.webservice", "org.apache.cxf.fediz.examples", "org.apache.cxf.fediz.examples.wsclientWebapp", "org.apache.cxf.fediz.systests.webapps"]
+                                    for candidate_group_id in possible_group_ids:
+                                        if check_maven_central(candidate_group_id, extracted_artifact_id, version):
+                                            modified_group_id = candidate_group_id
+                                            break
+                                    else:
+                                        logging.warning(f"Failed to find artifact in Maven Central: {group_id}:{extracted_artifact_id}:{version}")
+                                        continue
 
                                 if group_id == "org.apache.hive" and extracted_artifact_id == "hive-webhcat":
                                     modified_group_id = "org.apache.hive.hcatalog"
@@ -134,18 +129,9 @@ def process_jnorm_summaries():
                                     elif 'drill-hive-exec-shaded' in reference_artifact_path:
                                         artifact_name = artifact_name.replace('-jar', '')
                                     
-                                    reference_size = get_artifact_size(str(version_dir), original_artifact_name, ArtifactSource.REFERENCE)
-                                    rebuild_size = get_artifact_size(str(version_dir), artifact_name, ArtifactSource.REBUILD)
-                                    reference_total_size += reference_size
-                                    rebuild_total_size += rebuild_size
-
                                     gav_to_artifact_map[gav].append({
                                         "name": original_artifact_name,
                                         "jNorm": artifact.get('jNorm'),
-                                        "reference_size": reference_size,
-                                        "rebuild_size": rebuild_size,
-                                        "reference_classfiles": total_classfiles(str(version_dir), original_artifact_name, ArtifactSource.REFERENCE),
-                                        "rebuild_classfiles": total_classfiles(str(version_dir), artifact_name, ArtifactSource.REBUILD)
                                     })
                                     rebuild_artifact_path = os.path.join(str(version_dir), ArtifactSource.REBUILD.value, artifact_name)
                                     gav_to_file_path_map[gav].append({
@@ -156,9 +142,9 @@ def process_jnorm_summaries():
                     except json.JSONDecodeError:
                         continue
 
-    return gav_to_artifact_map, reference_total_size, rebuild_total_size, gav_to_file_path_map
+    return gav_to_artifact_map, gav_to_file_path_map
 
-gav_map, reference_total_size, rebuild_total_size, gav_to_file_path_map = process_jnorm_summaries()
+gav_map, gav_to_file_path_map = process_jnorm_summaries()
 
 unique_maven_central_releases = set()
 for artifact in gav_map.keys():
@@ -174,7 +160,5 @@ with open('make-release-consistent.txt', 'w') as f:
     f.write(f'{len(gav_map)} unique GAVs found\n')
     f.write(f'{sum(len(artifacts) for artifacts in gav_map.values())} artifacts found\n')
     f.write(f'{len(unique_maven_central_releases)} unique Maven Central releases found\n')
-    f.write(f'Reference total size: {reference_total_size / 1024} KB\n')
-    f.write(f'Rebuild total size: {rebuild_total_size / 1024} KB\n')
     f.write(f"GAV mapping saved to gav_to_artifact_map.json\n")
     f.write(f"GAV to file path mapping saved to gav_to_file_path_map.json\n")

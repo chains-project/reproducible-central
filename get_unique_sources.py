5a77550f0f5effa072e512a64c0a76e4218bf3aa
Aman Sharma <mannu.poski10@gmail.com>
2024-12-24 17:39:06 +0100

    get only sources of leaf nodes

--- get_unique_sources.py
+++ get_unique_sources.py
@@ -4,27 +4,31 @@ from pathlib import Path
 import os
 import json
 from collections import defaultdict
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Process release consistency')
+    parser.add_argument('--base-dir', 
+                       default="results",
+                       help='Base directory for processing (default: results)')
+    return parser.parse_args()
 
 def get_unique_sources(diffoscope_file):
     sources = defaultdict(int)
     with open(diffoscope_file, 'r') as file:
         data = json.load(file)
     
-    # Add top-level sources
-    if 'source1' in data:
-        sources[data['source1']] += 1
-    if 'source2' in data:
-        sources[data['source2']] += 1
-    
     # Process nested details recursively
     def process_details(details):
         for detail in details:
-            if 'source1' in detail:
-                sources[detail['source1']] += 1
-            if 'source2' in detail:
-                sources[detail['source2']] += 1
             if 'details' in detail:
                 process_details(detail['details'])
+            else:
+                if 'source1' in detail:
+                    sources[detail['source1']] += 1
+                if 'source2' in detail:
+                    sources[detail['source2']] += 1
+
     
     if 'details' in data:
         process_details(data['details'])
@@ -32,10 +36,12 @@ def get_unique_sources(diffoscope_file):
     return sources
 
 if __name__ == "__main__":
+    args = parse_args()
+    base_dir = args.base_dir
     all_sources = defaultdict(int)
     
     # Walk through results directory
-    for root, dirs, files in os.walk("results"):
+    for root, dirs, files in os.walk(base_dir):
         for file in files:
             if file.endswith(".diffoscope.json"):
                 file_sources = get_unique_sources(os.path.join(root, file))

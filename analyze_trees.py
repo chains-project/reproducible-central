aa7f302076c557950dabccaae545a65436eceaaf
Aman Sharma <mannu.poski10@gmail.com>
2025-01-03 12:35:48 +0100

    check if diffoscope is javap/procyon

--- analyze_trees.py
+++ analyze_trees.py
@@ -10,6 +10,13 @@ import shutil
 PROCYON_TOOL = "procyon -ec"
 JAVAP_TOOL = "javap -verbose"
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='Process release consistency')
+    parser.add_argument('--base-dir', 
+                       default="results",
+                       help='Base directory for processing (default: results)')
+    return parser.parse_args()
+
 def analyze_diffoscope(diffoscope_file):
     with open(diffoscope_file, 'r') as file:
         data = file.read()
@@ -18,6 +25,9 @@ def analyze_diffoscope(diffoscope_file):
         print(diffoscope_file)
 
 if __name__ == "__main__":
-    for root, dirs, files in os.walk("trees"):
+    args = parse_args()
+    base_dir = args.base_dir
+    for root, dirs, files in os.walk(base_dir):
         for file in files:
-            analyze_diffoscope(os.path.join(root, file))
+            if file.endswith(".diffoscope.json"):
+                analyze_diffoscope(os.path.join(root, file))

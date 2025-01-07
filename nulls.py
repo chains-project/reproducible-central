7be1e35212149c901361d6c18b2c64bb41511070
Aman Sharma <mannu.poski10@gmail.com>
2025-01-07 16:38:46 +0100

    we have a different way to "make release consistent"

--- nulls.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('make-release-consistent-file-paths.json', 'r') as f:
-    data = json.load(f)
-
-for key, value in data.items():
-    for artifact in value:
-        if artifact['rebuild'] is None:
-            print(key)

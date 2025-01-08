0f2a08954ef9fec530760129051d91ae8b76df2d
Aman Sharma <mannu.poski10@gmail.com>
2025-01-08 16:51:36 +0100

    minify all diffoscope files

--- minify-diffoscope.py
+++ minify-diffoscope.py
@@ -14,29 +14,28 @@ def parse_args():
                        help='Base directory for processing (default: results)')
     return parser.parse_args()
 
-def minify_diffoscope(diffoscope_root, diffoscope_filename, base_dir):
-    _, group_id, artifact_id, version = diffoscope_root.split('/')
-    diffoscope_file_tree = os.path.join('trees', f'{group_id}/{artifact_id}/{version}/{diffoscope_filename}.txt')
+def minify_diffoscope(diffoscope_file_path, diffoscope_filename, base_dir, tree_g, tree_a, tree_v):
+    diffoscope_file_tree = os.path.join('trees', f'{tree_g}/{tree_a}/{tree_v}/{diffoscope_filename}.txt')
     with open(diffoscope_file_tree, 'r') as file:
         sources = get_and_sanitize_leaf_nodes(file.readlines())
 
     if len(sources) == 0:
         return
     
-    minified_diffoscope_dir = os.path.join(f'minified_{base_dir}', f'{group_id}/{artifact_id}/{version}')
+    minified_diffoscope_dir = os.path.join(f'minified_{base_dir}', f'{tree_g}/{tree_a}/{tree_v}')
     os.makedirs(minified_diffoscope_dir, exist_ok=True)
 
     # we don't iterate by idx because we know if the source is same under the same tool, it will most likely be the same
     for source in sources:
         minified_diffoscope_file = os.path.join(minified_diffoscope_dir, f'{source.replace("/", "_")}.diffoscope.json')
-        minify(source, os.path.join(diffoscope_root, diffoscope_filename), minified_diffoscope_file)
+        minify(source, diffoscope_file_path, minified_diffoscope_file)
 
-def minify(source, diffoscope_file, minified_diffoscope_file):
+def minify(source, diffoscope_file_path, minified_diffoscope_file):
     with open(minified_diffoscope_file, 'w') as file:
-        file.write(json.dumps(get_diffoscope_for_source(diffoscope_file, source), indent=2))
+        file.write(json.dumps(get_diffoscope_for_source(diffoscope_file_path, source), indent=2))
 
-def get_diffoscope_for_source(diffoscope_file, source):
-    with open(diffoscope_file, 'r') as file:
+def get_diffoscope_for_source(diffoscope_file_path, source):
+    with open(diffoscope_file_path, 'r') as file:
         data = json.load(file)
             
     return process_data(data, source)
@@ -77,7 +76,20 @@ if __name__ == "__main__":
     base_dir = args.base_dir
     os.makedirs(f'minified_{base_dir}', exist_ok=True)
     # Walk through results directory
-    for root, dirs, files in os.walk(base_dir):
-        for file in files:
-            if file.endswith(".diffoscope.json"):
-                minify_diffoscope(root, file, base_dir)
+
+    with open('unreproducible_maven_projects_to_releases.json', 'r') as f:
+        unreproducible_projects = json.load(f)
+
+    for project in unreproducible_projects:
+        project_name = project['name']
+        original_group_id, original_artifact_id, original_version = project_name.split(":")
+
+        for release in project['maven_releases']:
+            group_id, artifact_id, version = release['gav'].split(":")
+            
+            for unreproducible_artifact in release['unreproducible_artifacts']:
+                file_name = unreproducible_artifact
+
+                path_to_diffoscope_file = os.path.join(args.base_dir, original_group_id, original_artifact_id, original_version, file_name)
+
+                minify_diffoscope(path_to_diffoscope_file, file_name, base_dir, group_id, artifact_id, version)

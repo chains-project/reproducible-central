413933928ac88360db9882e30e3b88a2c1123395
Aman Sharma <mannu.poski10@gmail.com>
2025-01-14 12:27:31 +0100

    update plot

--- count-unique-maven-central-releases.py
+++ count-unique-maven-central-releases.py
@@ -2,20 +2,10 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
+from statistics import median, mean
+
 with open('unreproducible_maven_projects_to_releases.json', 'r') as f:
     input_data = json.load(f)
-
-ga_counts = {}
-for item in input_data:
-    for release in item["maven_releases"]:
-        gav = release["gav"]
-        ga = ":".join(gav.split(":")[:2])  # Extract GA by excluding the version
-        ga_counts[ga] = ga_counts.get(ga, 0) + 1
-
-# Group by the number of versions (values)
-version_distribution = {}
-for count in ga_counts.values():
-    version_distribution[count] = version_distribution.get(count, 0) + 1
 
 
 gav_counts = {}
@@ -30,53 +20,16 @@ for count in gav_counts.values():
 
 print(unreproducible_artifact_distribution)
 
+plt.bar(unreproducible_artifact_distribution.keys(), unreproducible_artifact_distribution.values())
 
-# Prepare data for violin plot
-violin_data = []
-for version_count, ga_count in version_distribution.items():
-    violin_data.extend([version_count] * ga_count)
+for i in unreproducible_artifact_distribution:
+    number_of_unreproducible_artifacts = i
+    freq = unreproducible_artifact_distribution[i]
 
-violin_data.sort()
+    plt.text(number_of_unreproducible_artifacts-0.15, freq+16, freq, fontsize=12,)
 
-violin_data1 = []
-for version_count, ga_count in unreproducible_artifact_distribution.items():
-    violin_data1.extend([version_count] * ga_count)
+plt.xlabel('# Non-reproducible artifacts')
+plt.ylabel('# Maven releases')
 
-violin_data1.sort()
-print(violin_data1)
-
-min_value = min(violin_data)
-max_value = max(violin_data)
-median_value = np.median(violin_data)
-mean_value = np.mean(violin_data)
-
-min_value1 = min(violin_data1)
-max_value1 = max(violin_data1)
-median_value1 = np.median(violin_data1)
-mean_value1 = np.mean(violin_data1)
-
-# Create the plot
-fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(6, 8))
-axs[0].violinplot([violin_data], orientation='horizontal', showextrema=True, showmeans=True)
-axs[1].violinplot([violin_data1], orientation='horizontal', showextrema=True, showmeans=True)
-
-# Set labels and title
-axs[0].set_yticks([1, 1.25])
-axs[0].set_yticklabels([f'{min(version_distribution.values())}', f'{max(version_distribution.values())}'])
-axs[0].set_ylabel("# Maven Modules")
-axs[0].set_xlabel("# Versions")
-
-axs[1].set_yticks([1, 1.25])
-axs[1].set_yticklabels([f'{min(unreproducible_artifact_distribution.values())}', f'{max(unreproducible_artifact_distribution.values())}'])
-axs[1].set_ylabel("# Maven Releases")
-axs[1].set_xlabel("# Artifacts")
-
-axs[0].text(max_value*0.86, 1.1, f"Max: {max_value}", fontsize=10, color="red")
-axs[0].text(mean_value*1.2, 1.07, f"Mean: {mean_value:.2f}", fontsize=10, color="blue")
-axs[0].text(median_value, 1.12, f"Median: {median_value}", fontsize=10, color="green")
-
-axs[1].text(max_value1*0.90, 1.1, f"Max: {max_value1}", fontsize=10, color="red")
-axs[1].text(mean_value1*1.2, 1.07, f"Mean: {mean_value1:.2f}", fontsize=10, color="blue")
-axs[1].text(median_value1, 1.12, f"Median: {median_value1}", fontsize=10, color="green")
-
-plt.savefig('number_of_releases_per_maven_module.pdf')
+plt.savefig('descriptive_stats.pdf')
+plt.show()

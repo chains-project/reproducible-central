f1700a5d7d564c3d1542b04be7b9ed73871709d2
Aman Sharma <mannu.poski10@gmail.com>
2025-03-10 18:00:54 +0100

    total releases

---
+++ total_releases.py
@@ -0,0 +1,40 @@
+import os
+import argparse
+import glob
+import json
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Process release consistency')
+    parser.add_argument('--base-dir', 
+                       default="results",
+                       help='Base directory for processing (default: release-counter)')
+    return parser.parse_args()
+
+def load_success_builds():
+    path_to_file = os.path.join('diffoscope-failures', "success_after_excluding_f_should_be_respected.txt")
+    with open(path_to_file, 'r') as f:
+        return set([line.strip() for line in f.readlines()])
+
+def get_GAV(path_to_output):
+    _, group_id, artifact_id, version = path_to_output.split(os.path.sep)
+    return f"{group_id}:{artifact_id}:{version}"
+
+def main():
+    number_of_releases = []
+    args = parse_args()
+    base_level = len(args.base_dir.split(os.path.sep))
+    success_builds = load_success_builds()
+    for root, dirs, files in os.walk(args.base_dir):
+        for directory in dirs:
+            current_level = len(root.split(os.path.sep))
+            if current_level - base_level == 2:
+                gav = get_GAV(os.path.join(root, directory))
+                if gav in success_builds:
+                    modules = os.path.join(root, directory, "output.txt")
+                    with open(modules, 'r') as f:
+                        number_of_releases.append(len(f.readlines()))
+    
+    print(f"Total number of modules: {sum(number_of_releases)}")
+
+if __name__ == "__main__":
+    main()

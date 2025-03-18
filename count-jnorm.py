ea4ed65b87620dec7d91a4c51af45d997b28d235
Aman Sharma <mannu.poski10@gmail.com>
2025-03-18 18:38:30 +0100

    add jNorm analysis

--- count-jnorm.py
+++ count-jnorm.py
@@ -25,9 +25,9 @@ FAILED_NORMALIZATION = 0
 ERROR_IN_NORMALIZATION = 0
 
 result = {
-    "successful_normalization": set(),
-    "failed_normalization": set(),
-    "error_in_normalization": set()
+    "successful_normalization": {},
+    "failed_normalization": {},
+    "error_in_normalization": {}
 }
 for root, dirs, files in os.walk(base_dir):
     for directory in dirs:
@@ -48,18 +48,34 @@ for root, dirs, files in os.walk(base_dir):
 
                     diffoscope_file_path = os.path.join(diffoscope_path, f"{file_name}.diffoscope.json")
 
+                    g,a,v = diffoscope_file_path.split("/")[1:4]
+
+                    diff_files = {
+                        "diffoscope_diff": diffoscope_file_path,
+                        "jnorm_diff": jnorm_file.replace('.json', '.diff')
+                    }
+
                     if reference_exit_code == 0 and rebuild_exit_code == 0 and diff_exit_code == 0:
                         if '-sources' in jnorm_file:
                             SOURCES += 1
                         else:
                             SUCCESSFUL_NORMALIZATION += 1
-                            result["successful_normalization"].add(diffoscope_file_path)
+                            if f'{g}:{a}:{v}' in result["successful_normalization"]:
+                                result["successful_normalization"][f'{g}:{a}:{v}'].append(diff_files)
+                            else:
+                                result["successful_normalization"][f'{g}:{a}:{v}'] = [diff_files]
                     elif reference_exit_code == 0 and rebuild_exit_code == 0 and diff_exit_code != 0:
                         FAILED_NORMALIZATION += 1
-                        result["failed_normalization"].add(diffoscope_file_path)
+                        if f'{g}:{a}:{v}' in result["failed_normalization"]:
+                            result["failed_normalization"][f'{g}:{a}:{v}'].append(diff_files)
+                        else:
+                            result["failed_normalization"][f'{g}:{a}:{v}'] = [diff_files]
                     else:
                         ERROR_IN_NORMALIZATION += 1
-                        result["error_in_normalization"].add(diffoscope_file_path)
+                        if f'{g}:{a}:{v}' in result["error_in_normalization"]:
+                            result["error_in_normalization"][f'{g}:{a}:{v}'].append(diff_files)
+                        else:
+                            result["error_in_normalization"][f'{g}:{a}:{v}'] = [diff_files]
 
 # Exclude these as they don't have javap or procyon
 print(f"Sources: {SOURCES}")
@@ -68,11 +84,6 @@ print(f"Failed normalization: {FAILED_NORMALIZATION}")
 print(f"Error in normalization: {ERROR_IN_NORMALIZATION}")
 
 with open('jnorm_result.json', 'w+') as f:
-    result = {
-        "successful_normalization": list(result["successful_normalization"]),
-        "failed_normalization": list(result["failed_normalization"]),
-        "error_in_normalization": list(result["error_in_normalization"])
-    }
     json.dump(result, f, indent=4)
 
             

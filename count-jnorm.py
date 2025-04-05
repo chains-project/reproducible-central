298356d28424a24b8b3060aaf58f6c8971e38ee6
Aman Sharma <mannu.poski10@gmail.com>
2025-04-05 19:48:32 +0200

    get oss-rebuild results

--- count-jnorm.py
+++ count-jnorm.py
@@ -11,15 +11,14 @@ def parse_args():
                        help='Base directory for processing (default: release-counter)')
     return parser.parse_args()
 
-diffoscope_files_with_jvm = 'diffoscope_files_with_jvm.txt'
-with open(diffoscope_files_with_jvm, 'r') as f:
-    diffoscope_files_with_jvm = [i.strip().split("/")[4].replace('.diffoscope.json', '') for i in f.readlines()]
+diffoscope_files_with_all = 'diffoscope_files_with_all.txt'
+with open(diffoscope_files_with_all, 'r') as f:
+    diffoscope_files_with_all = [i.strip().split("/")[4].replace('.diffoscope.json', '') for i in f.readlines()]
 
 
 args = parse_args()
 base_dir = args.base_dir
 
-SOURCES = 0
 SUCCESSFUL_NORMALIZATION = 0
 FAILED_NORMALIZATION = 0
 ERROR_IN_NORMALIZATION = 0
@@ -31,7 +30,7 @@ result = {
 }
 for root, dirs, files in os.walk(base_dir):
     for directory in dirs:
-        if directory == 'jnorm':
+        if directory == 'oss-rebuild':
             jnorm_files = glob.glob(os.path.join(root, directory, '*.json'))
             for jnorm_file in jnorm_files:
                 with open(jnorm_file, 'r') as f:
@@ -40,7 +39,7 @@ for root, dirs, files in os.walk(base_dir):
                     rebuild_exit_code = jnorm['rebuild']
                     diff_exit_code = jnorm['diff']
                     
-                    if jnorm_file.split("/")[5].replace('.json', '') not in diffoscope_files_with_jvm:
+                    if jnorm_file.split("/")[5].replace('.json', '') not in diffoscope_files_with_all:
                         continue
 
                     file_name = jnorm_file.split("/")[5].replace('.json', '')
@@ -52,18 +51,15 @@ for root, dirs, files in os.walk(base_dir):
 
                     diff_files = {
                         "diffoscope_diff": diffoscope_file_path,
-                        "jnorm_diff": jnorm_file.replace('.json', '.diff')
+                        "oss_rebuild_diff": jnorm_file.replace('.json', '.diff')
                     }
 
                     if reference_exit_code == 0 and rebuild_exit_code == 0 and diff_exit_code == 0:
-                        if '-sources' in jnorm_file:
-                            SOURCES += 1
+                        SUCCESSFUL_NORMALIZATION += 1
+                        if f'{g}:{a}:{v}' in result["successful_normalization"]:
+                            result["successful_normalization"][f'{g}:{a}:{v}'].append(diff_files)
                         else:
-                            SUCCESSFUL_NORMALIZATION += 1
-                            if f'{g}:{a}:{v}' in result["successful_normalization"]:
-                                result["successful_normalization"][f'{g}:{a}:{v}'].append(diff_files)
-                            else:
-                                result["successful_normalization"][f'{g}:{a}:{v}'] = [diff_files]
+                            result["successful_normalization"][f'{g}:{a}:{v}'] = [diff_files]
                     elif reference_exit_code == 0 and rebuild_exit_code == 0 and diff_exit_code != 0:
                         FAILED_NORMALIZATION += 1
                         if f'{g}:{a}:{v}' in result["failed_normalization"]:
@@ -78,12 +74,11 @@ for root, dirs, files in os.walk(base_dir):
                             result["error_in_normalization"][f'{g}:{a}:{v}'] = [diff_files]
 
 # Exclude these as they don't have javap or procyon
-print(f"Sources: {SOURCES}")
 print(f"Successful normalization: {SUCCESSFUL_NORMALIZATION}")
 print(f"Failed normalization: {FAILED_NORMALIZATION}")
 print(f"Error in normalization: {ERROR_IN_NORMALIZATION}")
 
-with open('jnorm_result.json', 'w+') as f:
+with open('oss_rebuild_result.json', 'w+') as f:
     json.dump(result, f, indent=4)
 
             
